3d57082fb0d8c74bff64c1f59a73ce111208d7cd
박병관 <byeonggwan.park@lge.com>
2020-12-08 18:14:02 +0900

    post position 수정

--- lib/hangul.py
+++ lib/hangul.py
@@ -15,15 +15,13 @@ def han_iga(word):
     return tossi.pick(word, '이(가)')
 
 def han_ira(word):
-    if _is_jong(word):
-        return '이라'
-    return tossi.pick(word, '이라(가)')
+    return tossi.pick(word, '이라(라)')
+
 def han_obj(word):
     return tossi.pick(word, '을(를)')
 
 def han_un(word):
-    return tossi.pick(word, '은()')
-
+    return tossi.pick(word, '은(는)')
 
 def han_wa(word):
     return tossi.pick(word, '과(와)')
@@ -46,22 +44,21 @@ def postPosition(line, name):
         return line
     e = line.find(')')
     pps = line[s:e + 1]
-    pp = tossi.postfix(name, pps)
+    sep = pps.find('/')
+    pp1 = pps[1:sep]
+    pp2 = pps[sep + 1:-1]
+    pp = tossi.pick(name, f"{pp1}({pp2})")
     return line.replace(pps, pp)
 
 
 def postPosition1(line):
     s = line.find('(')
-    if s == -1 or s < 2:
+    if s == -1:
         return line
     e = line.find(')')
     pps = line[s:e + 1]
-    for i in range(1, len(line) - s):
-        a = ord(line[s - i])
-        if a >= 0xA1 and a <= 0xFE:
-            s = s + 1 - i
-            break
-    if s - 2 < 0:
-        return line
-    pp = tossi.postfix(line[:s], pps)
+    sep = pps.find('/')
+    pp1 = pps[1:sep]
+    pp2 = pps[sep + 1:-1]
+    pp = tossi.pick(line[:s], f"{pp1}({pp2})")
     return line.replace(pps, pp, 1)
